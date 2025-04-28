b42ce41865764911af0ad15a8d3b797b288bda3a
Felipe Eduardo Cabello Condor <felipeecc@hotmail.com>
2025-04-28 13:16:05 -0500

    cambios tablas basicas

---
+++ MVCBase/Controllers/AparienciaTelaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBase.Controllers
+{
+    public class AparienciaTelaController : Controller
+    {
+        // GET: AparienciaTela
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
